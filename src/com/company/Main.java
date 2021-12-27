0d86f236728731083292762bc1f2881c7b104a04
Nik25-77 <Kulman_77@mail.ru>
2021-12-27 19:54:47 +0700

    add a

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+    private Tank tank;
+    public static void main(String[] args) {
+        int a=0;
+
+        Math.abs(5);
+        System.currentTimeMillis();
+    }
+}
